dfa12a7dfa2a699c610c8c78889fd28fdeee73eb
stevemini <stevemini@steve-minis-Mac-mini.local>
2018-08-12 20:20:21 +0530

    no message

--- src/environments/firebaseConfig.ts
+++ src/environments/firebaseConfig.ts
@@ -1,8 +1,19 @@
+//Prod
+// export const FireBaseConfig = {
+//   apiKey: "AIzaSyCHOdXv4JXKfoLqF3arW3mF4ICXnV8WLDs",
+//   authDomain: "msp-online.firebaseapp.com",
+//   databaseURL: "https://msp-online.firebaseio.com",
+//   projectId: "msp-online",
+//   storageBucket: "",
+//   messagingSenderId: "964172443679"  
+// };
+
+//Dev
 export const FireBaseConfig = {
-  apiKey: "XXXXXXXXXXX",
-  authDomain: "XXXXXXXXXXX.firebaseapp.com",
-  databaseURL: "https://XXXXXXXXXXX.firebaseio.com",
-  projectId: "XXXXXXXXXXX",
-  storageBucket: "XXXXXXXXXXX.appspot.com",
-  messagingSenderId: "XXXXXXXXXXX"
+  apiKey: "AIzaSyB4EC-UH6np58bDUAzB12PTaXNSD1Pu50U",
+  authDomain: "msp-online-dev.firebaseapp.com",
+  databaseURL: "https://msp-online-dev.firebaseio.com",
+  projectId: "msp-online-dev",
+  storageBucket: "msp-online-dev.appspot.com",
+  messagingSenderId: "519152477963"
 };
